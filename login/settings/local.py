b735c10da437bc1c097cafc58ee0a7d31579ea90
Jose-Miguel7 <74680114+Jose-Miguel7@users.noreply.github.com>
2020-12-11 22:25:29 -0300

    'Secret_Key'

---
+++ login/settings/local.py
@@ -0,0 +1,21 @@
+from .base import *
+
+
+
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+    	'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    	'NAME': get_secret('DB_NAME'),
+    	'USER': get_secret('USER'),
+    	'PASSWORD': get_secret('PASSWORD'),
+ 		'HOST': 'localhost',
+ 		'PORT': '8080',
+    }
+}
+
+STATIC_URL = '/static/'
